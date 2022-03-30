6de3ae8865211b9683134100275b9a695b77444f
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-03-30 15:54:27 +0600

    Added Some Work and little knowledge is dangerous

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.scss'
 import Home from './Components/Home/Home'
-// import Contact from './components/Contact'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Components/Layout/Layout'
 import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<Layout></Layout>}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
